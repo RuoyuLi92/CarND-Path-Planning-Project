0158e4da35819e8334f874fc81ab4a2ce658f13c
Ruoyu <ruoyuli.kit@gmail.com>
2020-01-12 16:44:53 +0100

    constant speed and keep lane maneuver, debug vector accessor

--- src/main.cpp
+++ src/main.cpp
@@ -111,9 +111,9 @@ int main() {
                      map_waypoints_x, 
                      map_waypoints_y)
 			  //next_x_vals.push_back(car_x+(dist_inc*i)*cos(deg2rad(car_yaw)));
-			  next_x_vals.push_back(vec_xy(0));
+			  next_x_vals.push_back(vec_xy[0]);
 			  //next_y_vals.push_back(car_y+(dist_inc*i)*sin(deg2rad(car_yaw)));
-			  next_y_vals.push_back(vec_xy(1));
+			  next_y_vals.push_back(vec_xy[1]);
 			//std::cout << car_yaw;
 		  }
 		  
