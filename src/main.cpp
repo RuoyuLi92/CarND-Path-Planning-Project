d0177e8e5b4d9a213e868b61a9d2f01bae187b80
Ruoyu <ruoyuli.kit@gmail.com>
2020-01-12 12:38:19 +0100

    show me the yaw_rate

--- src/main.cpp
+++ src/main.cpp
@@ -104,6 +104,7 @@ int main() {
 		  for(int i = 0; i < 50; i++) {
 			next_x_vals.push_back(car_x+(dist_inc*i)*cos(deg2rad(car_yaw)));
 			next_y_vals.push_back(car_y+(dist_inc*i)*sin(deg2rad(car_yaw)));
+			std::cout << car_yaw;
 		  }
 		  
 		  // end
